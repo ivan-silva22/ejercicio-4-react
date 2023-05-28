0766cf9cc449ab388ab1acca140524b15072f687
Ivan Silva <silvaivan280@gmail.com>
2023-05-28 16:57:39 -0300

    agregue el componente ItemTarea donde se muestra las tareas

--- src/components/ListaTareas.jsx
+++ src/components/ListaTareas.jsx
@@ -1,7 +1,11 @@
+import ItemTarea from "./ItemTarea";
+
 function ListaTareas(){
     return(
-        <div>
-
+        <div className="bg-color col-sm-12 col-md-6 col-lg-5 my-3 m-auto ">
+            <ItemTarea></ItemTarea>
+            <ItemTarea></ItemTarea>
+            <ItemTarea></ItemTarea>
         </div>
     )
 }
