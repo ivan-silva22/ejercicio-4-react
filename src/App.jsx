6d78e199bb3610b18a63e0271eeca8942a78ee6e
Ivan Silva <silvaivan280@gmail.com>
2023-05-28 15:15:07 -0300

    agregue un nuevo componente FormularioTarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import FormularioTarea from './components/FormularioTarea'
 
 function App() {
   
   return (
-    <section>
-      
+    <section className='text-white container text-center my-4'>
+      <h1 className='display-3'>Lista de tareas</h1>
+      <hr />
+      <FormularioTarea></FormularioTarea>
     </section>  
   )
 }
